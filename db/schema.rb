f275fc7bd91727ba59828eedfe7589a84ace4237
Ashab-al <asxabal7@gmail.com>
2023-11-13 22:49:33 +0300

    create migration RemoveUserFromCategory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_174943) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_13_194849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,8 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_174943) do
     t.integer "sales"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "subscriptions", force: :cascade do |t|
@@ -48,7 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_174943) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "categories", "users"
   add_foreign_key "subscriptions", "categories"
   add_foreign_key "subscriptions", "users"
 end
