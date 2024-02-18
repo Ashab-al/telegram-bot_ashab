7c82bf30936b31d08467bc0dc6bb0219d4965598
Ashab-al <asxabal7@gmail.com>
2024-02-18 19:14:37 +0300

    added vacancy_controller protect_from_forgery

--- app/controllers/api/vacancy_controller.rb
+++ app/controllers/api/vacancy_controller.rb
@@ -1,5 +1,6 @@
 class Api::VacancyController < ApplicationController
   skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
 
   def index
     @vacancys = Vacancy.all
