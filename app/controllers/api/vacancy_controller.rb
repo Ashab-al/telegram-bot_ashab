c7d82161904d46bb4fb420339bb570358fbecc44
Ashab-al <asxabal7@gmail.com>
2024-02-18 19:11:03 +0300

    added skip_before_action

--- app/controllers/api/vacancy_controller.rb
+++ app/controllers/api/vacancy_controller.rb
@@ -1,6 +1,6 @@
 class Api::VacancyController < ApplicationController
-  protect_from_forgery with: :null_session
-  
+  skip_before_action :verify_authenticity_token
+
   def index
     @vacancys = Vacancy.all
     render json: @vacancys
