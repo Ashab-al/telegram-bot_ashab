18f457d7c8341ef2d6f810be5ac8dc104b925c20
Ashab <asxabal7@gmail.com>
2024-11-04 18:03:09 +0300

    fix: created secrets.yml.samle and commented yookassa

--- config/initializers/yookassa.rb
+++ config/initializers/yookassa.rb
@@ -1,4 +1,4 @@
-Yookassa.configure do |config|
-  config.shop_id = Rails.application.secrets.yookassa_api[:shop_id] 
-  config.api_key = Rails.application.secrets.yookassa_api[:api_key] 
-end
+# Yookassa.configure do |config|
+#   config.shop_id = Rails.application.secrets.yookassa_api[:shop_id] 
+#   config.api_key = Rails.application.secrets.yookassa_api[:api_key] 
+# end
