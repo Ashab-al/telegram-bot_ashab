91b405b7f340ae20155383c8932389962b967e0f
Ashab <asxabal7@gmail.com>
2025-01-01 13:57:25 +0300

    fix: formation_text

--- app/interactors/tg/total_vacancies_interactor.rb
+++ app/interactors/tg/total_vacancies_interactor.rb
@@ -10,13 +10,7 @@ class Tg::TotalVacanciesInteractor  < ActiveInteraction::Base
     text = I18n.t('vacancy.total_vacancies.all_vacancies_size', size: Vacancy.count)
 
     categories.each do |category|
-      text += if category.vacancies.size.positive?
-                I18n.t('vacancy.total_vacancies.category_and_vacancy_size_bold', 
-                        name: category.name, size: category.vacancies.size)
-              else
-                I18n.t('vacancy.total_vacancies.category_and_vacancy_size', 
-                  name: category.name, size: category.vacancies.size)
-              end
+      text += I18n.t('vacancy.total_vacancies.category_and_vacancy_size', name: category.vacancies.size.positive? ? "<b>#{category.name}</b>" : category.name, size: category.vacancies.size)
     end
 
     text
