e24209d584261ba158c3aeeacc6266c490895d6b
Ashab <asxabal7@gmail.com>
2025-01-04 18:38:38 +0300

    fix: edit vacancies_info

--- app/interactors/tg/total_vacancies_interactor.rb
+++ app/interactors/tg/total_vacancies_interactor.rb
@@ -3,7 +3,7 @@ class Tg::TotalVacanciesInteractor  < ActiveInteraction::Base
   def execute    
     {
       rows: Category.pluck(Arel.sql("name"), Arel.sql("(SELECT count(*) FROM vacancies WHERE vacancies.category_id = categories.id)")), 
-      vacancy_size: Vacancy.count
+      vacancies_size: Vacancy.count
     }
   end
 end
--- spec/interactors/tg/total_vacancies_spec.rb
+++ spec/interactors/tg/total_vacancies_spec.rb
@@ -4,13 +4,13 @@ require 'rails_helper'
 RSpec.describe Tg::TotalVacanciesInteractor do
   
   describe "#execute" do 
-    let(:vacancy_size) {rand(1..4)}
+    let(:vacancies_size) {rand(1..4)}
     let!(:category) { create(:category) }
-    let!(:vacancies) { create_list(:vacancy, vacancy_size, category_title: category.name, category_id: category.id) }
+    let!(:vacancies) { create_list(:vacancy, vacancies_size, category_title: category.name, category_id: category.id) }
     let(:outcome) { described_class.run() }
 
     it 'return correct size' do
-      expect(outcome.result[:vacancy_size]).to eq(vacancy_size)
+      expect(outcome.result[:vacancy_size]).to eq(vacancies_size)
     end
 
     it 'return correct category count' do 
