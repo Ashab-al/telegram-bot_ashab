90b20f548de0f38dcd4464127dae62914e539cae
Ashab <asxabal7@gmail.com>
2025-02-12 04:01:41 +0300

    fix/create interactor user/by_platform_id

--- app/interactors/tg/user/by_platfomr_id_interactor.rb
+++ app/interactors/tg/user/by_platfomr_id_interactor.rb
@@ -1,2 +1,26 @@
 class Tg::User::ByPlatformIdInteractor < ActiveInteraction::Base
+  integer :id, presence: true
+  string :username, default: ""
+  string :name, default: ""
+  integer :point, default: 0
+  integer :bonus, default: 5
+
+
+  def execute
+    user = User.find_by(platform_id: id)
+
+    unless user
+      new_user = User.new(
+        name: name,
+        username: username,
+        platform_id: id,
+        point: point,
+        bonus: bonus
+      ).save
+
+      return new_user
+    end
+
+    user
+  end
 end
