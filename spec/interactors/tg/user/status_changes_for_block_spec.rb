7bf945a0529cf464b869749efb57b50275ab8049
Ashab <asxabal7@gmail.com>
2025-02-17 16:27:00 +0300

    fix/create spec status_changes_for_block_spec

---
+++ spec/interactors/tg/user/status_changes_for_block_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Tg::User::StatusChangesForBlock do 
+
+  describe "#execute" do
+    let(:user) { create(:user) }
+
+    let(:change_status) { described_class.run(user: user) }
+
+    it "return success edit user status" do 
+      except(outcome.result.user.bot_status).to (User::BOT_STATUS_BLOCKED)
+    end
+  end
+end
