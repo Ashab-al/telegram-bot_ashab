01704bbf7c78dfb553c43e66577d500f13db099e
Ashab <asxabal7@gmail.com>
2025-02-18 03:03:19 +0300

    fix/create spec subscribe_to_category_spec

---
+++ spec/interactors/tg/user/subscribe_to_category_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+
+RSpec.describe Tg::User::SubscribeToCategoryInteractor do 
+
+  describe "#execute" do
+    let(:one_subscription) { 1 }
+    let(:category_size) { rand(2..10) }
+    let(:user) { create(:user) }
+    let(:categories) { create_list(:category_for_list, category_size) }
+
+    let(:subscription) { described_class.run(user: user, category: categories.first) }
+
+    it "return correct create subscription" do 
+      expect(subscription.result.category).to eq(categories.first) 
+    end
+
+    it "return correct count subscription" do
+      expect(subscription.result.subscriptions).to eq(one_subscription)
+    end
+  end
+end
