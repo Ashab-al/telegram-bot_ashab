9ea3470ee745574031cf2f59451e1de06afbfa21
Ashab <asxabal7@gmail.com>
2025-02-22 20:05:14 +0300

    fix/edit locale

--- app/renderers/buttons/with_all_tarifs_renderer.rb
+++ app/renderers/buttons/with_all_tarifs_renderer.rb
@@ -1,15 +1,15 @@
 class Buttons::WithAllTarifsRenderer
   TARIFS_PRICES = {
-    10 => 30, 
+    10 => 31, 
     30 => 85, 
     50 => 135, 
     100 => 255, 
-    150 => 360, 
+    150 => 362, 
     200 => 450
   }
 
   def call
-    TARIFS_PRICES.keys.map do | tarif, price | 
+    TARIFS_PRICES.map do | tarif, price | 
       @tarif = tarif
       @price = price
       [{ text: Tg::Common.erb_render("points/tarif_name", binding), callback_data: Tg::Common.erb_render("points/tarif_callback", binding) }]
