d14957df7820b190418b4f6b19487e8c525d3114
Ashab <asxabal7@gmail.com>
2025-02-24 20:11:20 +0300

    update Tg::User::UpdatePointsInteractor and spec

--- spec/interactors/tg/user/update_points_spec.rb
+++ spec/interactors/tg/user/update_points_spec.rb
@@ -4,13 +4,18 @@ require 'rails_helper'
 RSpec.describe Tg::User::UpdatePointsInteractor do 
   describe "#execute" do 
     let(:user) { create(:user) }
-
     let(:points) { rand(1..10) }
+    let(:stars) { points * 2 }
+    let(:one) { 1 }
 
-    let(:update) { described_class(user: user, points: points) }
+    let(:update) { described_class.run(user: user, points: points, stars: stars) }
+
+    before do
+      allow(Tg::SendInfoAboutNewPaymentInteractor).to receive(:run)
+    end
 
     it "return correct update points" do 
-      expect(update.result.points).to eq(points)
+      expect(update.result.point).to eq(one + points)
     end
   end
 end
