0ad4d7a78398a8573223d6a12faf8085794d79b4
Ashab <77648621+Ashab-al@users.noreply.github.com>
2025-03-01 21:10:22 +0300

    Fix delete unused (#50)
    * fix/comment gems
    
    * fix/delete GraphQl
    
    * fix/delete gem tomo and graphql
    
    * fix/delete gem yookassa and rest-client
    
    * fix/delete webpacker.yml
    
    * fix/delete tailwind
    
    * fix/delete gems sidekiq
    
    * fix/delete gems importmap-rails, turbo-rails, stimulus-rails
    
    * fix/delete assets
    
    * fix/delete channels and helpers
    
    * fix/delete app/javascript
    
    * fix/delete jobs
    
    * fix/delete mailers
    
    * fix/clear views
    
    * fix/delete config/webpack and package.json
    
    * fix/delete yarn.lock
    
    * fix/delete postcss.config.js
    
    * fix/delete bin/webpack
    
    * fix/delete babel.config.js browserslistrc
    
    * fix/delete sprockets-rails
    
    * fix/delete gem i18n
    
    * fix/delete manifest, importmap
    
    * fix/delete pagination_service

--- config/initializers/yookassa.rb
+++
@@ -1,4 +0,0 @@
-# Yookassa.configure do |config|
-#   config.shop_id = Rails.application.secrets.yookassa_api[:shop_id] 
-#   config.api_key = Rails.application.secrets.yookassa_api[:api_key] 
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   telegram_webhook TelegramWebhooksController
   
-  post "/graphql", to: "graphql#execute"
-  
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: "/graphql" if Rails.env.development?
-
-  root 'users#index'
-
   namespace :api do
     post 'users/mail_all', to: 'users#mail_all'
     resources :users, only: [:index, :show] do 
