b63a2f45dd78d689ff82d5c838257a5daa870233
Ashab <asxabal7@gmail.com>
2025-03-02 09:19:54 +0300

    fix/delete to_s in black_list_check_interactor

--- app/interactors/api/vacancy/black_list_check_interactor.rb
+++ app/interactors/api/vacancy/black_list_check_interactor.rb
@@ -3,7 +3,7 @@ class Api::Vacancy::BlackListCheckInteractor < ActiveInteraction::Base
   string :contact_information, presence: true
   
   def execute
-    blacklist = Blacklist.where(contact_information: contact_information).or(Blacklist.where(contact_information: platform_id.to_s))
+    blacklist = Blacklist.where(contact_information: contact_information).or(Blacklist.where(contact_information: platform_id))
     
     return errors.add(:blacklist, I18n.t("error.messages.error_validate_vacancy")) unless blacklist.empty?
 
